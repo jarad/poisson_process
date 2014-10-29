ca63a8bd691f29d5b8c624d247475b98b3ad3ff6
Jarad Niemi <niemi@iastate.edu>
2014-10-29 09:13:51 -0500

    copy server.R and ui.R from another applet

---
+++ ui.R
@@ -0,0 +1,34 @@
+require(shiny)
+require(markdown)
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("Null hypothesis probability conditional on an observed pvalue"),
+  
+  sidebarPanel(
+    #style="min-width:235px;max-width:275px",
+    sliderInput('pvalue', 'Observed pvalue',
+                min=.001, max=0.05, value=0.05, step=.001, round=FALSE),
+    br(),
+    sliderInput('pH0', 'Proportion of true null hypotheses',
+                min=0, max=1,    value=0.5,  step=0.1,  round=FALSE),
+    br(),
+    
+    h4('Alternative distribution'),
+    selectInput('distribution', '', 'normal', 'normal'),
+    numericInput('mean', 'Mean', value=0),
+    numericInput('sd', 'Standard deviation', value=1, min=0),
+    br(),
+    h4('Simulation parameters'),
+    numericInput('n', 'Number of simulations', 100, min=1, step=1),
+    numericInput('max_sims', 'Maximum attempts per simulation', 1000, min=1, step=1),
+    submitButton('Simulate.')
+  ),
+    
+  mainPanel(
+    tabsetPanel(
+      tabPanel('Results', plotOutput('plot'), textOutput('text')),
+      tabPanel('Help', includeMarkdown('help.md'))
+    )
+  )
+))
